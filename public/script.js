9323dc22ce0608c0b73812184fb086fc67f05a5b
maka819 <shironeko819@gmail.com>
2025-04-16 01:06:10 +0900

    Add the Clipboard buttons.

--- public/script.js
+++ public/script.js
@@ -27,3 +27,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// クリップボードにコピー
+function copyToClipboard(button) {
+  const textarea = button.closest(".terminal").querySelector(".copy-target");
+  textarea.select();
+  document.execCommand("copy");
+
+   // パスを差し替える（チェックマーク）
+  const path = button.querySelector("svg path");
+  const originalD = path.getAttribute("d");
+  const checkD = "M9 5H7a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2M9 5a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2M9 5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2m-6 9l2 2 4-4";
+  path.setAttribute("d", checkD);
+  path.style.stroke = "#00ffcc";
+
+    // copy-textのテキストを変更
+    const copyTextEl = button.querySelector(".copy-text");
+    const originalText = copyTextEl.textContent;
+    copyTextEl.textContent = "Copied!";
+
+    // 1秒後に元に戻す
+    setTimeout(() => {
+      path.setAttribute("d", originalD);
+      path.style.stroke = "currentColor";
+      copyTextEl.textContent = originalText;
+    }, 1000);
+}
