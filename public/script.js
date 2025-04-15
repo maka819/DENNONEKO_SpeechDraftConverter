e3ded37b1da86124e9d96a7e5a8f1d5c9350672d
maka819 <shironeko819@gmail.com>
2025-04-16 00:28:41 +0900

    Adjust CSS style's and Add sidebar.

--- public/script.js
+++ public/script.js
@@ -1,16 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const textareas = document.querySelectorAll("textarea");
-  
-    textareas.forEach(textarea => {
-      const resize = () => {
-        textarea.style.height = "auto"; // 一旦リセット
-        textarea.style.height = textarea.scrollHeight + "px"; // 内容に合わせて再設定
-      };
-  
-      // 初期状態でフィット
-      resize();
-  
-      // 入力中もリアルタイムでフィット
-      textarea.addEventListener("input", resize);
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  const textareas = document.querySelectorAll("textarea");
+
+  textareas.forEach(textarea => {
+    // --- textareaの自動リサイズ処理 ---
+    const resize = () => {
+      textarea.style.height = "auto"; // 一旦リセット
+      textarea.style.height = textarea.scrollHeight + "px"; // 内容に合わせて再設定
+    };
+
+    // 初期状態でフィット
+    resize();
+
+     // 入力中もリアルタイムでフィット
+    textarea.addEventListener("input", resize);
+
+    // --- textareaフォーカス時のハイライト処理 ---
+    const parent = textarea.closest(".wrap"); //textareaから一番近い.wrapを取得
+    if (parent) {
+      textarea.addEventListener("focus", () => {
+        parent.classList.add("is-focused");
+      });
+
+      textarea.addEventListener("blur", () => {
+        parent.classList.remove("is-focused");
+      });
+    }
   });
+});
